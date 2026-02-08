6494ca21dbc1ab5bff0311592bbb20511c4bb01d
michael-maltsev <36999014+michael-maltsev@users.noreply.github.com>
2026-02-08 23:42:22 +0200

    Revert "Optimize SAP queries"
    This reverts commit 8cd58adf21a2171bd4e891c9a7e0b49d7339b229.
    
    Didn't seem to have any effect on the query speed.

--- courses_to_json.py
+++ courses_to_json.py
@@ -29,8 +29,6 @@ CACHE_DIR: Optional[Path] = (
 
 VERBOSE_LOGGING = False
 
-SAP_BASE_URL = "https://portalex.technion.ac.il/sap/opu/odata/sap/Z_CM_EV_CDIR_DATA_SRV/"
-
 session = requests.session()
 
 session.proxies = {
@@ -61,38 +59,71 @@ def send_request_once(query: str, allow_empty: bool):
     if VERBOSE_LOGGING:
         print(f"Sending request: {query}")
 
-    url = SAP_BASE_URL + query
+    url = "https://portalex.technion.ac.il/sap/opu/odata/sap/Z_CM_EV_CDIR_DATA_SRV/$batch?sap-client=700"
 
     headers = {
-        "Accept": "application/json",
-        "Accept-Language": "he",
-        "DataServiceVersion": "2.0",
-        "MaxDataServiceVersion": "2.0",
-        "X-Requested-With": "X",
-        "sap-cancel-on-close": "true",
-        "sap-contextid-accept": "header",
+        # "Host": "portalex.technion.ac.il",
+        # "Connection": "keep-alive",
+        # "Content-Length": "955",
         "sec-ch-ua": '"Not/A)Brand";v="8", "Chromium";v="126", "Brave";v="126"',
+        "MaxDataServiceVersion": "2.0",
+        "Accept-Language": "he",
         "sec-ch-ua-mobile": "?0",
-        "sec-ch-ua-platform": '"Windows"',
-        "Sec-GPC": "1",
-        "Sec-Fetch-Site": "same-origin",
-        "Sec-Fetch-Mode": "cors",
-        "Sec-Fetch-Dest": "empty",
         "User-Agent": (
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like"
             " Gecko) Chrome/126.0.0.0 Safari/537.36"
         ),
+        "Content-Type": "multipart/mixed;boundary=batch_1d12-afbf-e3c7",
+        "Accept": "multipart/mixed",
+        "sap-contextid-accept": "header",
+        "sap-cancel-on-close": "true",
+        "X-Requested-With": "X",
+        "DataServiceVersion": "2.0",
+        # "SAP-PASSPORT": SAP_PASSPORT,
+        "sec-ch-ua-platform": '"Windows"',
+        "Sec-GPC": "1",
+        "Origin": "https://portalex.technion.ac.il",
+        "Sec-Fetch-Site": "same-origin",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Dest": "empty",
         "Referer": "https://portalex.technion.ac.il/ovv/",
+        # "Accept-Encoding": "gzip, deflate, br, zstd",
+        # "Cookie": SAP_COOKIE,
     }
 
-    response = session.get(url, headers=headers, timeout=REQUEST_TIMEOUT)
-    if response.status_code != 200:
-        raise RuntimeError(f"Bad status code: {response.status_code}, expected 200")
+    data = f"""
+--batch_1d12-afbf-e3c7
+Content-Type: application/http
+Content-Transfer-Encoding: binary
 
-    result = response.json()
+GET {query} HTTP/1.1
+sap-cancel-on-close: true
+X-Requested-With: X
+sap-contextid-accept: header
+Accept: application/json
+Accept-Language: he
+DataServiceVersion: 2.0
+MaxDataServiceVersion: 2.0
+
+
+--batch_1d12-afbf-e3c7--
+"""
+    data = data.replace("\n", "\r\n")
+
+    response = session.post(url, headers=headers, data=data, timeout=REQUEST_TIMEOUT)
+    if response.status_code != 202:
+        raise RuntimeError(f"Bad status code: {response.status_code}, expected 202")
+
+    response_chunks = response.text.replace("\r\n", "\n").strip().split("\n\n")
+    if len(response_chunks) != 3:
+        raise RuntimeError(f"Invalid response: {response_chunks}")
+
+    json_str = response_chunks[2].split("\n", 1)[0]
 
     if VERBOSE_LOGGING:
-        print(f"Got {len(response.text)} bytes")
+        print(f"Got {len(json_str)} bytes")
+
+    result = json.loads(json_str)
 
     if not allow_empty and result == {"d": {"results": []}}:
         raise RuntimeError("Empty response")
@@ -199,6 +230,12 @@ def get_sap_course_numbers(year: int, semester: int):
 def get_sap_course(year: int, semester: int, course: str):
     params = {
         "sap-client": "700",
+        # "$skip": "0",
+        # "$top": "50",
+        # "$orderby": "Short asc",
+        "$filter": (
+            f"Peryr eq '{year}' and Perid eq '{semester}' and Otjid eq '{course}'"
+        ),
         "$select": ",".join(
             [
                 "Otjid",
@@ -223,11 +260,14 @@ def get_sap_course(year: int, semester: int, course: str):
                 "SmPrereq",
             ]
         ),
+        # "$inlinecount": "allpages",
     }
-    raw_data = send_request(
-        f"SmObjectSet(Otjid='{course}',Peryr='{year}',Perid='{semester}',ZzCgOtjid='',ZzPoVersion='',ZzScOtjid='')?{urllib.parse.urlencode(params)}"
-    )
-    return raw_data["d"]
+    raw_data = send_request(f"SmObjectSet?{urllib.parse.urlencode(params)}")
+    results = raw_data["d"]["results"]
+    if len(results) != 1:
+        raise RuntimeError(f"Invalid results for {course}: {raw_data}")
+
+    return results[0]
 
 
 @cache
@@ -307,15 +347,6 @@ def get_event_schedule_info(
         "$filter": (
             f"Otjid eq '{event_schedule_id}' and Peryr eq '{year}' and Perid eq"
             f" '{semester}'"
-        ),
-        "$select": ",".join(
-            [
-                "Evdat",
-                "Beguz",
-                "Enduz",
-                "Rooms",
-                "Persons",
-            ]
         ),
         "$expand": ",".join(
             [
@@ -601,13 +632,6 @@ def get_course_schedule(year: int, semester: int, course_number: str):
     """Get the schedule for a course."""
     params = {
         "sap-client": "700",
-        "$select": ",".join(
-            [
-                "ZzSeSeqnr",
-                "Name",
-                "EObjectSet",
-            ]
-        ),
         "$expand": ",".join(
             [
                 "EObjectSet",
